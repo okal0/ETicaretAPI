26911f73e4e9e38fe0ad6d0fdf1cf0ccf5e0bfb9
Oğulcan Kalafatoğlu <ghorse28206@gmail.com>
2024-11-01 10:09:04 +0300

    dummy GET, POST, PUT API methods.

--- Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -9,26 +9,27 @@ namespace ETicaretAPI.API.Controllers
     [Route("api/[controller]")]
     [ApiController]
     public class ProductsController : ControllerBase
-    { 
+    {
 
         readonly private IProductWriteRepository _productWriteRepository;
         readonly private IProductReadRepository _productReadRepository;
         public ProductsController(IProductWriteRepository productWriteRepository, IProductReadRepository productReadRepository)
         {
-        _productWriteRepository = productWriteRepository;
-        _productReadRepository = productReadRepository;
+            _productWriteRepository = productWriteRepository;
+            _productReadRepository = productReadRepository;
         }
         [HttpGet]
-        public async Task Get()
+        public async Task<ActionResult> Get()
         {
-            await _productWriteRepository.AddRangeAsync(new()
+            /*await _productWriteRepository.AddRangeAsync(new()
                 {
                     new() { Id = Guid.NewGuid(), Name = "Product11", Price = 100, CreatedDate = DateTime.Now, Stock = 15 },
                     new() { Id = Guid.NewGuid(), Name = "Product8", Price = 100, CreatedDate = DateTime.Now, Stock = 15 },
                     new() { Id = Guid.NewGuid(), Name = "Product9", Price = 100, CreatedDate = DateTime.Now, Stock = 15 },
                 }
                 );
-            await _productWriteRepository.SaveAsync();
+            await _productWriteRepository.SaveAsync();*/
+            return Ok(_productReadRepository.GetAll());
         }
 
         [HttpGet("{id}")]
@@ -38,5 +39,26 @@ namespace ETicaretAPI.API.Controllers
 
             return Ok(product);
         }
+
+        //modelli versiyonlara çevir bunları
+        [HttpPost]
+        public async Task<IActionResult> Post(Product product)
+        {
+            await _productWriteRepository.AddAsync(product);
+            await _productWriteRepository.SaveAsync();
+
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put(Product product)
+        {
+            _productWriteRepository.Update(product);
+            _productWriteRepository.SaveAsync();
+
+            return Ok();
+        }
+
+
     }
 }
